3439214650da73337cb86159486c779869da4b7e
Kot <o0kot0o@gmail.com>
2026-02-20 17:55:26 -0600

    v0.2a

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import binascii
 import re
 
-with open("WorldData.sav", 'rb') as f:
+save_file = "WorldData.sav"
+new_save_file = "WorldData_modified.sav"
+find_bytes = b'TotalPlaytime\x00\x0e\x00\x00\x00FloatProperty'
+replace_bytes = b'\x00\x00\x00\x00'
+
+print(f"Opening file {save_file}.")
+with open(save_file, 'rb') as f:
+    print(f"Reading from file {save_file}.")
     hex_data = bytearray(f.read())
 
-pattern = re.compile(b'TotalPlaytime\x00\x0e\x00\x00\x00FloatProperty')
+pattern = re.compile(find_bytes)
+
+print(f"Looking at {len(hex_data)} bytes for the offset.")
 for match in pattern.finditer(hex_data):
     offset = match.end() + 11
-    print(f"Offset found at: {offset} (0x{offset:X})")
-    print(binascii.hexlify(hex_data[offset:offset+16]).decode('utf-8'))
-    hex_data[offset:offset+2] = b'\xFA\xFA'
+    print(f"Found {offset}:{offset+4}.")
+    print(f" ---> {binascii.hexlify(hex_data[offset:offset+4]).decode('utf-8').upper()}")
+    print(f"Replacing bytes {offset}:{offset+4} with {replace_bytes}")
+    hex_data[offset:offset+4] = replace_bytes
 
-with open("WorldData_modified.sav", 'wb') as f_out:
+print(f"Saving file to {new_save_file}")
+with open(new_save_file, 'wb') as f_out:
     f_out.write(hex_data)
-
-
-
-        #print(binascii.hexlify(hex_data[match.end()+11:]).decode('utf-8'))
-#print(hex_data)
-
-
-# to Find b'\x54\x6F\x74\x61\x6C\x65\x00\x0E\x00\x00\x00\x46\x6C\x6F\x72\x74\x79'
